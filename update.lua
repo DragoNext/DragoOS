fe3dc8b790cd6e8815371200208a09d17adec01b
DragoNext <49029552+DragoNext@users.noreply.github.com>
2021-07-08 23:28:59 +0100

    update.lua updated
    getting updates from github to opencomputer using internet adapter

--- update.lua
+++ update.lua
@@ -1,1 +1,15 @@
 -- Update
+local wifi = require("internet")
+lnk = "http://raw.githubusercontent.com/DragoNext/DragoOS/main/"
+upd = {"dragoos.lua","update.lua"}
+for i = 1,2
+do 
+	local handle = wifi.request(lnk..upd[i])
+	local result = ""
+	for chunk in handle do result = result..chunk end 
+	print(upd[i].." Updated")
+	file = io.open(upd[i],"w")
+	file.write(result)
+	file.close()
+end 
+
