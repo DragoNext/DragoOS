b3b24c9e0013ba9e15244d7c69539ed4791f04ce
DragoNext <49029552+DragoNext@users.noreply.github.com>
2021-07-09 14:55:48 +0100

    Update dragoos.lua

--- dragoos.lua
+++ dragoos.lua
@@ -1,1 +1,35 @@
 -- dragos
+local gpu = require("gpu")
+local internet = require("internet")
+local io = require("io")
+
+
+local screen_adress = gpu.getScreen() -- Current Screen #
+local max_color_depth = gpu.maxDepth() -- Current max color depth (depends on screen)
+
+local screen_xsize = gpu.getResolution()[0] -- Width 
+local screen_ysize = gpu.getResolution()[1]
+
+local dragoos_theme = 0 -- Theme index 
+
+local dragoos_double_buffering = false -- turned on or off 
+local dragoos_
+
+function switch_buffering () 
+	if dragoos_double_buffering is false then 
+		dragoos_double_buffering = true 
+	else
+		dragoos_double_buffering = false 
+	end 
+end 
+
+function os_startup ()
+	
+	for i = 1,screen_ysize 
+	do 
+		gpu.fill(0,0+i,screen_xsize,1)
+	end 
+end 
+
+os_startup()
+
