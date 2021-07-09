8b8c42d549288ca1bf9ee8523bf135015ece09d6
DragoNext <49029552+DragoNext@users.noreply.github.com>
2021-07-09 15:19:19 +0100

    Update update.lua

--- update.lua
+++ update.lua
@@ -2,13 +2,14 @@
 local wifi = require("internet")
 lnk = "https://raw.githubusercontent.com/DragoNext/DragoOS/main/"
 upd = {"https://pastebin.com/raw/bArVCdqZ",lnk.."update.lua"}
+names = {"dragoos.lua","update.lua"}
 for i = 1,2
 do 
 	local handle = wifi.request(upd[i])
 	local result = ""
 	for chunk in handle do result = result..chunk end 
 	print(upd[i].." Updated")
-	file = io.open(upd[i],"w")
+	file = io.open(names[i],"w")
 	file:write(result)
 	file:close()
 end 
