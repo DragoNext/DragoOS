28a36d900348ddcbd713700b509b38aa7a380f7b
DragoNext <49029552+DragoNext@users.noreply.github.com>
2022-01-07 23:16:47 +0000

    Update update.lua
    Bezuzyteczne

--- update.lua
+++ update.lua
@@ -1,6 +1,5 @@
 -- Update
 local wifi = require("internet")
-lnk = "https://raw.githubusercontent.com/DragoNext/DragoOS/main/"
 upd = {"https://rawcdn.githack.com/DragoNext/DragoOS/34287c7e6fa27b5a34d892af64547256f369d715/dragoos.lua" ,"https://rawcdn.githack.com/DragoNext/DragoOS/34287c7e6fa27b5a34d892af64547256f369d715/update.luas"}
 names = {"dragoos.lua","update.lua"}
 for i = 1,2
